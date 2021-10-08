df8ba81e2ff8f8e2c57e6372055093bde5412d10
Abishek-wayne <abishekwayne@gmail.com>
2021-10-08 22:08:59 +0530

    Condition JSX Expressions

--- React-App/My-First-React-App/public/scripts/App.js
+++ React-App/My-First-React-App/public/scripts/App.js
@@ -1,11 +1,86 @@
 'use strict';
 
 //JavaScript XML
+
+var book = {
+    title: 'The Immortals of Meluah',
+    subtitle: 'Where a man becomes a God',
+    options: ['One', 'Two', 'Three']
+};
+
 var template = React.createElement(
-  'p',
-  null,
-  'I am Working Hahahaha Again! HHEHEHEH'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        book.title
+    ),
+    book.subtitle && React.createElement(
+        'p',
+        null,
+        book.subtitle
+    ),
+    displayOptions(),
+    book.options && book.options.length > 0 ? 'Here are your options' : 'No Options'
 );
+
+function displayOptions() {
+    if (book.options && book.options.length > 0) {
+        return React.createElement(
+            'p',
+            null,
+            'Here are your options',
+            book.options
+        );
+    } else {
+        return React.createElement(
+            'p',
+            null,
+            'There are no options available :('
+        );
+    }
+}
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Abishek Dinesh'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 23'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: Hyderabad'
+    ),
+    React.createElement(
+        'h2',
+        null,
+        'Hobbies'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Playing Games'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Reading Books'
+        )
+    )
+);
+
 var docDiv = document.getElementById('react-div');
 
 ReactDOM.render(template, docDiv);
--- React-App/My-First-React-App/public/src/App.js
+++ React-App/My-First-React-App/public/src/App.js
@@ -1,5 +1,43 @@
 //JavaScript XML
-var template = <p>I am Working Hahahaha Again! HHEHEHEH</p>;
+
+var book = {
+    title : 'The Immortals of Meluah',
+    subtitle : 'Where a man becomes a God',
+    options: ['One','Two','Three']
+};
+
+var template = (
+    <div>
+    <h1>{book.title}</h1>
+     {book.subtitle && <p>{book.subtitle}</p>}
+    {displayOptions()}
+    {book.options && book.options.length > 0 ? 'Here are your options' : 'No Options' }
+    </div>
+);
+
+function displayOptions(){
+    if( book.options && book.options.length > 0 ){
+        return <p>Here are your options{book.options}</p>;
+    }else{
+        return <p>There are no options available :(</p>;
+    }
+}
+
+var templateTwo = (
+<div>
+    <h1>
+    Abishek Dinesh
+    </h1>
+    <p>Age: 23</p>
+    <p>Location: Hyderabad</p>
+    <h2>Hobbies</h2>
+    <ol>
+    <li>Playing Games</li>
+    <li>Reading Books</li>
+    </ol>
+</div>
+);
+
 var docDiv = document.getElementById('react-div');
 
 ReactDOM.render(template,docDiv);
