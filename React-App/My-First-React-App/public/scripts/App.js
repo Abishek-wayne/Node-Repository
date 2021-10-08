1a0e10b5ad9ee89a1c3ca2c13c2ae6104366b6a5
Abishek-wayne <abishekwayne@gmail.com>
2021-10-08 12:37:12 +0530

    Babel Compiler - Working

---
+++ React-App/My-First-React-App/public/scripts/App.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//JavaScript XML
+var template = React.createElement(
+  'p',
+  null,
+  'I am Working Hahahaha Again! HHEHEHEH'
+);
+var docDiv = document.getElementById('react-div');
+
+ReactDOM.render(template, docDiv);
---
+++ React-App/My-First-React-App/public/src/App.js
@@ -0,0 +1,5 @@
+//JavaScript XML
+var template = <p>I am Working Hahahaha Again! HHEHEHEH</p>;
+var docDiv = document.getElementById('react-div');
+
+ReactDOM.render(template,docDiv);
