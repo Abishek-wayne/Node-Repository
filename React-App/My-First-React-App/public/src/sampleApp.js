617f1d883fd4f30b06d35610e91b2f81daa3f596
Abishek-wayne <abishekwayne@gmail.com>
2021-10-21 21:57:11 +0530

    Webpack with babel loader

--- React-App/My-First-React-App/public/src/sampleApp.js
+++ React-App/My-First-React-App/public/src/sampleApp.js
@@ -1,1 +1,12 @@
-console.log('Webpack is running');
+// import getSenior, {isAdult,canDrink} from './person.js'
+
+// console.log('sampleApp.js is running');
+// console.log('isAdult ', isAdult(18));
+// console.log('canDrink ', canDrink(21));
+// console.log('isSenior ', getSenior(55));
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const sampleJsx = <p>Hello I will render something!</p>;
+ReactDOM.render( sampleJsx, document.getElementById('react-div') );
